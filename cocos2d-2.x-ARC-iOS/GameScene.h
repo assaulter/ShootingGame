69fea8888415a207539bff96ff041fb9b1f22460
久保 和貴 <k-kubo@biglobe.co.jp>
2013-02-19 21:16:52 +0900

    プレイヤーを設置。touchしたポイントに移動する。 Signed-off-by: 久保 和貴 <k-kubo@biglobe.co.jp>

---
+++ cocos2d-2.x-ARC-iOS/GameScene.h
@@ -0,0 +1,16 @@
+//
+//  GameScene.h
+//  ShootingGame
+//
+//  Created by KazukiKubo on 2013/02/19.
+//  Copyright 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameScene : CCLayer {
+    
+}
+
+@end
---
+++ cocos2d-2.x-ARC-iOS/GameScene.m
@@ -0,0 +1,14 @@
+//
+//  GameScene.m
+//  ShootingGame
+//
+//  Created by KazukiKubo on 2013/02/19.
+//  Copyright 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameScene.h"
+
+
+@implementation GameScene
+
+@end
